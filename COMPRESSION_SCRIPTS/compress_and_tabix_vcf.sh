4eff2291411a3c4bebe7d0de982070369aa5b0d3
Kurt Hetrick <khetric1@jhmi.edu>
2020-03-25 12:07:19 -0400

    refactored compress_and_tabix_vcf.sh to be consistent with current production pipelines

--- COMPRESSION_SCRIPTS/compress_and_tabix_vcf.sh
+++ COMPRESSION_SCRIPTS/compress_and_tabix_vcf.sh
@@ -29,15 +29,29 @@
 	TABIX_EXEC=$3
 	BGZIP_EXEC=$4
 
-mkdir -p $DIR_TO_PARSE/MD5_REPORTS/
-
 START_COMPRESS_VCF=`date '+%s'`
 
-	$BGZIP_EXEC -c $IN_VCF > $IN_VCF.gz && $TABIX_EXEC -h $IN_VCF.gz
+	# if any part of pipe fails set exit to non-zero
 
-	rm -f $IN_VCF".idx"
+		set -o pipefail
+
+	# compress vcf with bgzip and create tbi index
+
+		$BGZIP_EXEC -c $IN_VCF > $IN_VCF.gz && $TABIX_EXEC -h $IN_VCF.gz
+
+	# check the exit signal at this point.
+
+		SCRIPT_STATUS=`echo $?`
+
+	# delete the tribble index
+
+		rm -f $IN_VCF".idx"
 
 END_COMPRESS_VCF=`date '+%s'`
 
-echo $IN_VCF,COMPRESS_AND_INDEX_VCF,$START_COMPRESS_VCF,$END_COMPRESS_VCF \
+echo $IN_VCF,COMPRESS_AND_INDEX_VCF,$HOSTNAME,$START_COMPRESS_VCF,$END_COMPRESS_VCF \
 >> $DIR_TO_PARSE/COMPRESSOR.TEST.WALL.CLOCK.TIMES.csv
+
+# exit with the signal from the program
+
+	exit $SCRIPT_STATUS
